bb3384cdc5d48e65879c68f9720e36d788bbdd82
dxvmxdx <dxvmxdx@gmail.com>
2026-07-14 16:55:16 +0900

    feat: Add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import styles from './App.module.css';
 import Header from './components/Header/Header';
 import Todos from './components/Todos/Todos';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 function App() {
   return (
-    <>
+    <DarkModeProvider>
       <h1 className={styles.title}>To Do List</h1>
       <div className={styles.container}>
         <Header />
         <Todos />
       </div>
-    </>
+    </DarkModeProvider>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import { MdNightlight, MdLightMode } from 'react-icons/md';
+
 import styles from './Header.module.css';
+import { useDarkMode } from '../../context/DarkModeContext';
 
 export default function Header() {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   const today = new Date();
   const FormattedDate = `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`;
 
   return (
     <header className={styles.header}>
-      <button className={styles.button}>
-        <MdNightlight />
+      <button className={styles.button} onClick={toggleDarkMode}>
+        {!darkMode && <MdNightlight />}
+        {darkMode && <MdLightMode />}
       </button>
       <p>{FormattedDate}</p>
     </header>
